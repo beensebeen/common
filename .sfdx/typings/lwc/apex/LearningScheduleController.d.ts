d675303e04aa449814c81059e04842cf4854c80a
beensebeen <zkdi1007@gmail.com>
2025-08-09 15:31:16 +0900

    Delete .sfdx directory

--- .sfdx/typings/lwc/apex/LearningScheduleController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/LearningScheduleController.getSubscriptions" {
-  export default function getSubscriptions(param: {contactId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/LearningScheduleController.createEvents" {
-  export default function createEvents(param: {contactId: any, productId: any, weekday: any, startTime: any, durationMinutes: any, repeatUntil: any}): Promise<any>;
-}
